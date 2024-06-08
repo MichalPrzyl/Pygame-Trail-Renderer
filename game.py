500838851089bb1cd78dc62a6116fa099583fd4f
Michal Przylucki <michal.przyl@gmail.com>
2024-06-09 00:55:58 +0200

    Cleanup

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+
+
 WIDTH, HEIGHT = (400,400)
 
 # Define Colors 
@@ -9,7 +11,7 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-## initialize pygame and create window
+## Initialize pygame and create window
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("MP GAME")
@@ -43,34 +45,43 @@ squares = []
 SQUARE_QUANTITY = 150
 
 def add_square(new_x_position, new_y_position, r, g, b):
-    squares.append(
-        {'position': 
-         {
-             'x': new_x_position, 
-             'y': new_y_position
-         },
-         'color': (r, g, b)})
+    data = {
+        'position': {'x': new_x_position, 'y': new_y_position},
+        'color': (r, g, b)
+    }
+
+    squares.append(data)
+
+    # That is the number of trial instances quantity. FIFO-type structure.
+    # When new square should appear, remove one that lasts the longest.
     if len(squares) > SQUARE_QUANTITY:
         squares.pop(0)
 
-iteration = 0
+loop_iteration = 0
 
+# You can experiment with different value to receive different gradients of
+# trail colors.
 
+# Red color
 color_r = 210 
 color_r_going_up = True
 
+# Green color
 color_g = 150
 color_g_going_up = True
 
+# Blue color
 color_b = 90 
 color_b_going_up = True
 
+# How fast rgba values are changing.
 COLOR_CHANGE_STEP = 0.06
 
 while running:
-    iteration += 1
-    # Drawing whole canvas
-    pygame.draw.rect(screen, BLACK, (0, 0, 400, 400))
+    loop_iteration += 1
+
+    # Clearning canvas
+    pygame.draw.rect(screen, BLACK, (0, 0, 400, 400))  # TODO: Use global variables.
 
     horizontal_step = x_step if going_right else -x_step
     vertical_step = y_step if going_down else -y_step
@@ -88,12 +99,17 @@ while running:
     if position['y'] < UP_THRESHOLD + PLAYER_WIDTH:
         going_down = True
 
-    if iteration % 60 == 0:
+    # We can't add "trail element" after every frame because
+    # that would be too many. We have to reduce that.
+    if loop_iteration % 60 == 0:
         add_square(new_x_position, new_y_position, color_r, color_g, color_b)
 
+
+    # Actual changing position of main square.
     set_x_position(new_x_position)
     set_y_position(new_y_position)
 
+    # Updating RBGA values.
     if color_r > 252:
         color_r_going_up = False
     if color_r < 2:
@@ -117,7 +133,7 @@ while running:
     color_g += color_g_step
     color_b += color_b_step
 
-    # Drawing trail
+    # Drawing trail.
     for square in squares:
         pygame.draw.rect(screen, 
                          (square['color'][0], 
@@ -128,9 +144,11 @@ while running:
                          PLAYER_WIDTH, PLAYER_WIDTH))
 
     # Drawing player
-    pygame.draw.rect(screen, (color_r, color_g,color_b), (position['x'] - PLAYER_WIDTH, position['y'] - PLAYER_WIDTH, PLAYER_WIDTH, PLAYER_WIDTH))
+    pygame.draw.rect(
+        screen, (color_r, color_g,color_b),
+        (position['x'] - PLAYER_WIDTH, position['y'] - PLAYER_WIDTH, 
+         PLAYER_WIDTH, PLAYER_WIDTH))
 
-    #1 Process input/events
     # clock.tick(FPS)     ## will make the loop run at the same speed all the time
     for event in pygame.event.get():  # gets all the events which have occured till now and keeps tab of them.
         ## listening for the the X button at the top
