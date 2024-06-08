45b31c0d3bc6a1aa6ea3d69c8ba16f5ac5f72589
Michal Przylucki <michal.przyl@gmail.com>
2024-06-09 00:27:40 +0200

    Working trail, nice colors + screen readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Trail renderer in pygame
+
+## Abstract
+Just simple implementation to see some nice patterns rendered.
+
+## Screens
+Single squared trail.
+
+![Screen01](.readme/screen01.png)
--- game.py
+++ game.py
@@ -21,7 +21,7 @@ running = True
 # Moving
 position = {'x': 75, 'y': 55}
 x_step = 0.09
-y_step = 0.06
+y_step = 0.04
 
 def set_x_position(value):
     position['x'] = value
@@ -42,12 +42,31 @@ PLAYER_WIDTH = 20
 squares = []
 SQUARE_QUANTITY = 150
 
-def add_square(new_x_position, new_y_position):
-    squares.append({'position': {'x': new_x_position, 'y': new_y_position}})
+def add_square(new_x_position, new_y_position, r, g, b):
+    squares.append(
+        {'position': 
+         {
+             'x': new_x_position, 
+             'y': new_y_position
+         },
+         'color': (r, g, b)})
     if len(squares) > SQUARE_QUANTITY:
         squares.pop(0)
 
 iteration = 0
+
+
+color_r = 210 
+color_r_going_up = True
+
+color_g = 150
+color_g_going_up = True
+
+color_b = 90 
+color_b_going_up = True
+
+COLOR_CHANGE_STEP = 0.06
+
 while running:
     iteration += 1
     # Drawing whole canvas
@@ -70,21 +89,46 @@ while running:
         going_down = True
 
     if iteration % 60 == 0:
-        add_square(new_x_position, new_y_position)
+        add_square(new_x_position, new_y_position, color_r, color_g, color_b)
 
     set_x_position(new_x_position)
     set_y_position(new_y_position)
 
+    if color_r > 252:
+        color_r_going_up = False
+    if color_r < 2:
+        color_r_going_up = True
+
+    if color_g > 252:
+        color_g_going_up = False
+    if color_g < 2:
+        color_g_going_up = True
+
+    if color_b > 252:
+        color_b_going_up = False
+    if color_b < 2:
+        color_b_going_up = True
+
+    color_r_step = COLOR_CHANGE_STEP if color_r_going_up else -COLOR_CHANGE_STEP
+    color_g_step = COLOR_CHANGE_STEP if color_g_going_up else -COLOR_CHANGE_STEP
+    color_b_step = COLOR_CHANGE_STEP if color_b_going_up else -COLOR_CHANGE_STEP
+            
+    color_r += color_r_step
+    color_g += color_g_step
+    color_b += color_b_step
+
     # Drawing trail
     for square in squares:
         pygame.draw.rect(screen, 
-                         BLUE,
+                         (square['color'][0], 
+                          square['color'][1], 
+                          square['color'][2]),
                          (square['position']['x'] - PLAYER_WIDTH,
                           square['position']['y'] - PLAYER_WIDTH, 
                          PLAYER_WIDTH, PLAYER_WIDTH))
 
     # Drawing player
-    pygame.draw.rect(screen, GREEN, (position['x'] - PLAYER_WIDTH, position['y'] - PLAYER_WIDTH, PLAYER_WIDTH, PLAYER_WIDTH))
+    pygame.draw.rect(screen, (color_r, color_g,color_b), (position['x'] - PLAYER_WIDTH, position['y'] - PLAYER_WIDTH, PLAYER_WIDTH, PLAYER_WIDTH))
 
     #1 Process input/events
     # clock.tick(FPS)     ## will make the loop run at the same speed all the time
